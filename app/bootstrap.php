b84033c9e50917f2f7c2605b8618ebdee8a61152
Martijn Endenburg <martijn.endenburg@gmail.com>
2013-12-18 18:38:25 +0100

    Added display startup errors

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,7 @@
 date_default_timezone_set( 'Europe/Amsterdam' );
 
 error_reporting( -1 );
+ini_set( 'display_startup_errors', 1 );
 ini_set( 'display_errors', '1' );
 
 if ( !defined( 'ROOT_DIR' ) ) {
