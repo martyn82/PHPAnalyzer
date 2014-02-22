20c779d2a60f050255256c679ad813bb854f066f
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-02-22 20:46:45 +0100

    Improved test coverage

--- test/lib/Mend/Metrics/Report/Partition/MethodPartitionTest.php
+++ test/lib/Mend/Metrics/Report/Partition/MethodPartitionTest.php
@@ -2,18 +2,37 @@
 namespace Mend\Metrics\Report\Partition;
 
 use Mend\Source\Code\Model\MethodArray;
+use Mend\Parser\Node\PHPNode;
+use Mend\Network\Web\Url;
+use Mend\Source\Code\Model\Method;
 
 class MethodPartitionTest extends \TestCase {
 	public function testAccessors() {
 		$absolute = mt_rand( 0, PHP_INT_MAX );
 		$relative = (float) mt_rand( 1, PHP_INT_MAX ) / PHP_INT_MAX;
-		$methods = $this->getMock( '\Mend\Source\Code\Model\MethodArray' );
+
+		$method = $this->getMock( '\Mend\Source\Code\Model\Method', array(), array(), '', false );
+		$methods = new MethodArray();
+		$methods[] = $method;
 
 		$partition = new MethodPartition( $absolute, $relative, $methods );
 
 		self::assertEquals( $absolute, $partition->getAbsolute() );
 		self::assertEquals( $relative, $partition->getRelative() );
 		self::assertEquals( $methods, $partition->getMethods() );
+
+		$expectedArray = array(
+			'absolute' => $partition->getAbsolute(),
+			'relative' => $partition->getRelative(),
+			'methods' => array_map(
+				function ( Method $method ) {
+					return $method->toArray();
+				},
+				(array) $methods
+			)
+		);
+
+		self::assertEquals( $expectedArray, $partition->toArray() );
 	}
 
 	public function testEmpty() {
