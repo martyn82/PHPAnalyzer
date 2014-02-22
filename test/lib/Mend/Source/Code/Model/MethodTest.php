66f03c954b881b8434928f0cb4a1690613a7cb8d
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-02-22 13:33:34 +0100

    Improved test coverage

--- test/lib/Mend/Source/Code/Model/MethodTest.php
+++ test/lib/Mend/Source/Code/Model/MethodTest.php
@@ -2,6 +2,8 @@
 namespace Mend\Source\Code\Model;
 
 use Mend\Network\Web\Url;
+use Mend\Metrics\Complexity\ComplexityResult;
+use Mend\Metrics\UnitSize\UnitSizeResult;
 use Mend\Source\Code\Location\SourceUrl;
 
 class MethodTest extends \TestCase {
@@ -10,8 +12,24 @@ class MethodTest extends \TestCase {
 		$sourceUrl = new SourceUrl( Url::createFromString( 'file:///tmp/foo#(1,0),(10,10)' ) );
 
 		$method = new Method( $node, $sourceUrl );
+		$unitSize = new UnitSizeResult( 1, 1 );
+		$complexity = new ComplexityResult( 1, 1 );
+
+		$method->unitSize( $unitSize );
+		$method->complexity( $complexity );
 
 		self::assertEquals( $node, $method->getNode() );
 		self::assertEquals( $sourceUrl, $method->getSourceUrl() );
+		self::assertEquals( $unitSize, $method->unitSize() );
+		self::assertEquals( $complexity, $method->complexity() );
+
+		$expectedArray = array(
+			'name' => $node->getName(),
+			'location' => $sourceUrl->__toString(),
+			'unitSize' => $unitSize->toArray(),
+			'complexity' => $complexity->toArray()
+		);
+
+		self::assertEquals( $expectedArray, $method->toArray() );
 	}
 }
