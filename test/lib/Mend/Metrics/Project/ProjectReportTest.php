5f181cacb8326b3dbf85c1fc4feac9ad1633e598
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-02-22 22:46:22 +0100

    Improved tests

--- test/lib/Mend/Metrics/Project/ProjectReportTest.php
+++ test/lib/Mend/Metrics/Project/ProjectReportTest.php
@@ -6,11 +6,25 @@ class ProjectReportTest extends \TestCase {
 		$root = $this->getMock( '\Mend\IO\FileSystem\Directory', array(), array( '/tmp' ) );
 		$project = $this->getMock( '\Mend\Metrics\Project\Project', array(), array( 'name', 'key', $root ) );
 		$report = $this->getMock( '\Mend\Metrics\Report\Report', array(), array() );
+		$dateTime = new \DateTime();
 
-		$projectReport = new ProjectReport( $project );
+		$projectReport = new ProjectReport( $project, $dateTime );
 		$projectReport->addReport( 'report1', $report );
 
 		self::assertEquals( $report, $projectReport->getReport( 'report1' ) );
+		self::assertEquals( $project, $projectReport->getProject() );
+		self::assertEquals( $dateTime, $projectReport->getDateTime() );
+
+		self::assertTrue( $projectReport->hasReport( 'report1' ) );
+		self::assertFalse( $projectReport->hasReport( 'foo' ) );
+
+		$expectedArray = array(
+			'project' => $project->toArray(),
+			'dateTime' => $dateTime->format( 'r' ),
+			'report1' => $report->toArray()
+		);
+
+		self::assertEquals( $expectedArray, $projectReport->toArray() );
 	}
 
 	/**
