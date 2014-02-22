5c3721a959ac3ac50c1e53034705cd356f3bccd5
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-02-22 23:11:42 +0100

    Somewhat improved test

--- test/lib/Mend/Metrics/Project/ProjectReaderTest.php
+++ test/lib/Mend/Metrics/Project/ProjectReaderTest.php
@@ -1,16 +1,44 @@
 <?php
 namespace Mend\Metrics\Project;
 
-use Mend\IO\FileSystem\Directory;
+require_once realpath( __DIR__ . "/../../IO/Stream" ) . "/FileStreamTest.php";
 
-class ProjectReaderTest extends \TestCase {
-	public function testGetFiles() {
+use Mend\IO\FileSystem\Directory;
+use Mend\IO\Stream\FileStreamTest;
+
+class ProjectReaderTest extends FileStreamTest {
+	public function testConstructor() {
 		$root = new Directory( '/tmp/foo' );
 
 		$project = $this->getMock( '\Mend\Metrics\Project\Project', array( 'getRoot' ), array( 'Foo', 'foo', $root ) );
-		$project->expects( self::any() )->method( 'getRoot' )->will( self::returnValue( $root ) );
+
+		$project->expects( self::any() )
+			->method( 'getRoot' )
+			->will( self::returnValue( $root ) );
 
 		$reader = new ProjectReader( $project );
 		self::assertNotNull( $reader );
+	}
+
+	public function testGetFiles() {
+		$root = $this->getMock( '\Mend\IO\FileSystem\Directory', array(), array( '/foo' ), '', false );
+
+		$root->expects( self::any() )
+			->method( 'getName' )
+			->will( self::returnValue( '/foo' ) );
+
+		$project = $this->getMock( '\Mend\Metrics\Project\Project', array( 'getRoot' ), array( 'foo', 'bar', $root ) );
+
+		$project->expects( self::any() )
+			->method( 'getRoot' )
+			->will( self::returnValue( $root ) );
+
+		$reader = $this->getMock(
+			'\Mend\Metrics\Project\ProjectReader',
+			array(),
+			array( $project )
+		);
+
+		$reader->getFiles();
 	}
 }
