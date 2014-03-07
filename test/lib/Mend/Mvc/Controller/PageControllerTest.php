274f7c2552ce95b43c81bef800bdfc0059167544
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-03-07 23:07:29 +0100

    Extended coverage in tests

--- test/lib/Mend/Mvc/Controller/PageControllerTest.php
+++ test/lib/Mend/Mvc/Controller/PageControllerTest.php
@@ -4,17 +4,46 @@ namespace Mend\Mvc\Controller;
 use Mend\Network\Web\Url;
 
 class PageControllerTest extends \TestCase {
-	public function testDispatch() {
+	/**
+	 * @dataProvider switchProvider
+	 *
+	 * @param boloean $enableRender
+	 */
+	public function testDispatch( $enableRender ) {
 		$url = Url::createFromString( 'http://www.example.org/foo/bar' );
 
 		$request = $this->createRequest( $url );
 		$response = $this->createResponse( $url );
 		$factory = $this->createFactory();
 
+		$view = $this->createView();
+		$layout = $this->createLayout();
+
 		$controller = new DummyPageController( $request, $response, $factory );
+		$controller->setView( $view );
+		$controller->setLayout( $layout );
+
+		$controller->enableRender( $enableRender );
+		$controller->enableLayout( true );
+
+		$controller->setViewTemplatePath( 'views/' );
+		$controller->setLayoutTemplatePath( 'views/' );
+		$controller->setLayoutTemplate( 'default.phtml' );
+
 		$controller->dispatchAction( 'bar' );
 
 		self::assertEquals( 'bar', $controller->getActionName() );
+		self::assertEquals( $request, $controller->getRequest() );
+		self::assertEquals( $response, $controller->getResponse() );
+		self::assertEquals( $view, $controller->getView() );
+		self::assertEquals( $layout, $controller->getLayout() );
+	}
+
+	public function switchProvider() {
+		return array(
+			array( true ),
+			array( false )
+		);
 	}
 
 	/**
@@ -31,6 +60,14 @@ class PageControllerTest extends \TestCase {
 		$controller->dispatchAction( 'non' );
 
 		self::fail( "Test shoud have triggered an exception" );
+	}
+
+	private function createLayout() {
+		return $this->getMock( '\Mend\Mvc\View\Layout' );
+	}
+
+	private function createView() {
+		return $this->getMock( '\Mend\Mvc\View\View' );
 	}
 
 	private function createRequest( Url $url ) {
